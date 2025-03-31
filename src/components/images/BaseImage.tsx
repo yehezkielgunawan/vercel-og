5dc1f18f56a2290b9acb68630e1bdabdde6d5507
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2025-03-31 12:30:09 +0700

    style: format code for consistency and readability

--- src/components/images/BaseImage.tsx
+++ src/components/images/BaseImage.tsx
@@ -3,24 +3,24 @@ import React from "react";
 import clsxm from "@/lib/helpers/clsxm";
 
 type BaseImageProps = {
-  className?: string;
+	className?: string;
 } & React.ComponentPropsWithRef<"img">;
 
 const BaseImage = React.forwardRef<HTMLImageElement, BaseImageProps>(
-  ({ className, ...rest }, ref) => {
-    return (
-      // biome-ignore lint/a11y/useAltText: <explanation>
-      <img
-        aria-label="image"
-        ref={ref}
-        className={clsxm(className)}
-        alt={rest.alt}
-        loading="lazy"
-        decoding="async"
-        {...rest}
-      />
-    );
-  }
+	({ className, ...rest }, ref) => {
+		return (
+			// biome-ignore lint/a11y/useAltText: <explanation>
+			<img
+				aria-label="image"
+				ref={ref}
+				className={clsxm(className)}
+				alt={rest.alt}
+				loading="lazy"
+				decoding="async"
+				{...rest}
+			/>
+		);
+	},
 );
 
 export default BaseImage;
--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -4,13 +4,13 @@ import FooterComponent from "@/components/layouts/FooterComponent";
 import HeaderComponent from "@/components/layouts/HeaderComponent";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <section className="h-screen dark:bg-dark">
-      <HeaderComponent />
-      <div className="layout-container">{children}</div>
-      <FooterComponent />
-    </section>
-  );
+	return (
+		<section className="h-screen dark:bg-dark">
+			<HeaderComponent />
+			<div className="layout-container">{children}</div>
+			<FooterComponent />
+		</section>
+	);
 };
 
 export default Layout;
